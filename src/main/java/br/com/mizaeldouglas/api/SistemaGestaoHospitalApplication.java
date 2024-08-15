2357b7e83096098ddb9abe8a8cc01bd6d9b5d008
mizaeldouglas <mizaeldouglas2@gmail.com>
2024-08-15 16:28:12 -0300

    Initial commit

---
+++ src/main/java/br/com/mizaeldouglas/api/SistemaGestaoHospitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.mizaeldouglas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestaoHospitalApplication.class, args);
+	}
+
+}
