332fb9c159413c081833711d5c70d0eac452224b
mizaeldouglas <mizaeldouglas2@gmail.com>
2024-08-16 08:45:24 -0300

    feat: create PatientDTOs

---
+++ src/main/java/br/com/mizaeldouglas/api/dto/patient/PatientRequestDto.java
@@ -0,0 +1,10 @@
+package br.com.mizaeldouglas.api.dto.patient;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * DTO for {@link br.com.mizaeldouglas.api.domain.patient.Patient}
+ */
+public record PatientRequestDto(Long id, String cpf, String address, Date birthDate,String name,String email, String password, String phone) implements Serializable {
+}
