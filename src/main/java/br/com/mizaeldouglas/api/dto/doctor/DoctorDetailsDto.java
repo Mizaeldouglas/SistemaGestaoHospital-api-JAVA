78cc531cb9567da7c3c62242e162bd24a6f5fdd7
mizaeldouglas <mizaeldouglas2@gmail.com>
2024-08-17 17:28:38 -0300

    feat: deleted class users

--- src/main/java/br/com/mizaeldouglas/api/dto/doctor/DoctorDetailsDto.java
+++
@@ -1,6 +0,0 @@
-package br.com.mizaeldouglas.api.dto.doctor;
-
-import java.io.Serializable;
-
-public record DoctorDetailsDto(Long id, String crm, String specialty, String name, String email, String phone) implements Serializable {
-}
